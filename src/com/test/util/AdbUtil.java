a10a30e1309d4fa15380ad4c7555e050ccb86876
sailenxu <893849834@qq.com>
2017-12-07 22:44:36 +0800

    adbutil
    增加adb执行类，执行adb时使用

---
+++ src/com/test/util/AdbUtil.java
@@ -0,0 +1,46 @@
+package com.test.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 由于adb执行时，首次启动服务有启动信息，使用adb时需要把启动信息过滤掉
+ * @author sai
+ *
+ */
+public class AdbUtil {
+	private CmdTool cmdUtil = new CmdTool();
+	
+	/**
+	 * 执行adb devices指令，返回list集合，过滤启动信息
+	 * @param zhiling
+	 * @return
+	 */
+	public List<String> getListByADB(String zhiling){
+		List<String> adblist=new ArrayList<String>();
+		for(String s:cmdUtil.getListByCmd(zhiling)){
+			if (s.contains("daemon")) {
+				continue;
+			}else {
+				adblist.add(s);
+			}
+		}
+		return adblist;
+	}
+	/**
+	 * 获取当前路径，使用当前路径下的adb
+	 * @return
+	 */
+	final public String getAdbPath(){
+		String adbpath = System.getProperty("user.dir")+"\\adb.exe";
+		return adbpath;
+	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		AdbUtil adbUtil=new AdbUtil();
+//		for(String s:adbUtil.getListByADB("adb devices")){
+//			System.out.println(s+"6666");
+//		}
+//	}
+
+}
--- src/com/test/util/DeviceInfo.java
+++ src/com/test/util/DeviceInfo.java
@@ -1,5 +1,33 @@
 package com.test.util;
 
-public class DeviceInfo {
+import java.util.List;
 
+public class DeviceInfo {
+	private AdbUtil adbUtil = new AdbUtil();
+	/***
+	 * 获取设备的产品型号
+	 * @param deviceId
+	 * @return
+	 */
+	public String getOs(String deviceId){
+		String os="";
+		if (deviceId!=null&&deviceId!="") {
+			
+			CmdTool cmd=new CmdTool();
+			List<String> list=cmd.getListByCmd(adbUtil.getAdbPath()+" -s "+deviceId+" shell getprop ro.product.model");
+			for(String s:list){
+				if (s!=null&&s!="") {
+					if (s.contains("=")) {
+						String[] osSplit=s.split("=");
+						os=osSplit[1];
+						break;
+					}
+				}
+			}
+		}
+		if (os.equals("")||os==null) {
+			os="sorry，没获取到……";
+		}
+		return os;
+	}
 }
