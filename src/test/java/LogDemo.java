a452a22f9581ec4360d236658bc2af1c65f56a97
sai <893849834@qq.com>
2020-05-11 15:32:01 +0800

    修改log日志输出类，可以正常在前端打印log

--- src/test/java/LogDemo.java
+++ src/test/java/LogDemo.java
@@ -8,48 +8,39 @@ import javax.swing.JTextArea;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class LogDemo extends JFrame {
-    private JLabel logLabel;
+public class LogDemo {
     private JScrollPane logScrollPane;
     private JTextArea logTextArea;
     private final static Log log = LogFactory.getLog(LogDemo.class);
 
-    public LogDemo() {
-        logLabel = new javax.swing.JLabel();
-        logScrollPane = new javax.swing.JScrollPane();
-        logTextArea = new javax.swing.JTextArea();
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        logLabel.setText(" ");
-
-        logTextArea.setColumns(20);
-        logTextArea.setRows(5);
-        logScrollPane.setViewportView(logTextArea);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout.createSequentialGroup().addContainerGap().addComponent(logLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 610, Short.MAX_VALUE).addContainerGap()).addComponent(logScrollPane, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE));
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout.createSequentialGroup().addContainerGap().addComponent(logLabel).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED).addComponent(logScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE)));
-        pack();
+    public LogDemo(JTextArea logTextArea, JScrollPane logScrollPane) {
+        this.logScrollPane = logScrollPane;
+        this.logTextArea = logTextArea;
     }
 
     public void initLog() {
         try {
-            Thread t1, t2;
-            t1 = new LabelLogAppender(logLabel);
+            Thread t2;
             t2 = new TextAreaLogAppender(logTextArea, logScrollPane);
-            t1.start();
             t2.start();
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, e, "绑定日志输出组件错误", JOptionPane.ERROR_MESSAGE);
+//            JOptionPane.showMessageDialog(this, e, "绑定日志输出组件错误", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     public static void main(String[] s) {
-        LogDemo logDemoFrame = new LogDemo();
+        JFrame jFrame = new JFrame();
+        jFrame.setSize(500, 500);
+        JTextArea logta = new JTextArea();
+        JScrollPane logsp = new JScrollPane(logta);
+        logta.setColumns(20);
+        logta.setRows(5);
+        logsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        LogDemo logDemoFrame = new LogDemo(logta, logsp);
         logDemoFrame.initLog();
-        logDemoFrame.setVisible(true);
+        jFrame.add(logsp);
+        jFrame.setVisible(true);
         for (int i = 0; i < 1000; i++) {
             log.info("测试日志输出:" + i);
         }
